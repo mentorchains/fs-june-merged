02071a68b73c67c435930b4cfdc5c4e41c54beb7
someguy13 <ryan.kwong@gmail.com>
2021-07-27 15:48:46 -0700

    Updated Schema

--- reply-voting/app/vote.rb
+++ reply-voting/app/vote.rb
@@ -1,29 +1,16 @@
-class Votetore
+class VoteStore
     class << self
     # Returns all votes stored in database
       def get_votes
         PluginStore.get('reply_voting', 'votes') || {}
       end
-
-    # Returns all replies with counts
-      def get_count
-        PluginStore.get('reply_voting', 'vote_count') || {}
-      end
   
     # @param vote_id: Integer   Unique id for each vote
-    # @param vote: Hash         Contains the vote_id, reply_id, and user_id
+    # @param vote: Hash         Contains the reply_id, user_id, upvote/downvote
       def vote(vote_id, vote)
         votes = get_votes()
         votes[vote_id] = vote
         PluginStore.set('reply_voting', 'votes', votes)
-      end
-
-    # @param reply_id: Integer   Indicates which reply's count to update
-    # @param count: Hash         Contains the reply_id and updated count
-      def update_count(reply_id,count)
-        counts = get_count()
-        counts[reply_id] = count
-        PluginStore.set('reply_voting', 'vote_count', counts)
       end
 
     # @param vote_id: Integer   Indicates which vote to remove
@@ -35,3 +22,12 @@ class Votetore
 
     end
 end
+
+# == Schema Information
+#
+# Table name: votes
+#
+#  user_id    :integer          not null
+#  reply_id   :integer          not null
+#  upvote     :boolean          not null
+#

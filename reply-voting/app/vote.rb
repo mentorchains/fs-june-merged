c4cedd31df7a598ed68041f7c2a35648f8763e30
someguy13 <ryan.kwong@gmail.com>
2021-07-28 20:32:08 -0700

    Added vote count methods

--- reply-voting/app/vote.rb
+++ reply-voting/app/vote.rb
@@ -1,25 +1,43 @@
 class VoteStore
     class << self
-    # Returns all votes stored in database
-      def get_votes
-        PluginStore.get('reply_voting', 'votes') || {}
-      end
-  
-    # @param vote_id: Integer   Unique id for each vote
-    # @param vote: Hash         Contains the reply_id, user_id, upvote/downvote
-      def vote(vote_id, vote)
-        votes = get_votes()
-        votes[vote_id] = vote
-        PluginStore.set('reply_voting', 'votes', votes)
-      end
+        # Returns all votes stored in database
+        def get_votes
+            PluginStore.get('reply_voting', 'votes') || {}
+        end
+    
+        # @param vote_id: Integer   Unique id for each vote
+        # @param vote: Hash         Contains the reply_id, user_id, upvote/downvote
+        def vote(vote_id, vote)
+            votes = get_votes()
+            votes[vote_id] = vote
+            PluginStore.set('reply_voting', 'votes', votes)
+        end
 
-    # @param vote_id: Integer   Indicates which vote to remove
-      def remove_vote(vote_id)
-        votes = get_votes()
-        votes.delete(vote_id)
-        PluginStore.set('reply_voting', 'votes', votes)
-      end
+        # @param vote_id: Integer   Indicates which vote to remove
+        def remove_vote(vote_id)
+            votes = get_votes()
+            votes.delete(vote_id)
+            PluginStore.set('reply_voting', 'votes', votes)
+        end
 
+        # @param reply_id: Integer  Indicates which reply to get count for 
+        # @return count: Integer    Count of specified reply_id
+        def get_count(reply_id)
+            votes = get_votes().values
+            count = 0
+            votes.each do |vote|
+                if vote[:reply_id] == reply_id
+                    count += vote[:upvote] ? 1 : -1
+                end
+            end
+            count
+        end
+
+        # @return count: Hash    Counts of all reply_id
+        def get_all_count()
+            votes = get_votes().values
+            votes.each_with_object(Hash.new(0)) { |h1, h2| h2[h1[:reply_id]] += h1[:upvote] ? 1 : -1}
+        end
     end
 end
 

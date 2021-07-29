5f0881bbc969c8414bf04e66971523af66125d45
PyCoder49 <aryan-a@live.in>
2021-07-28 21:36:32 -0700

    cleaned up the whitespaces between lines

--- reply-voting/app/controllers/reply_voting_controller.rb
+++ reply-voting/app/controllers/reply_voting_controller.rb
@@ -17,33 +17,25 @@ class ReplyVotingController < ::ApplicationController
 	#
 	# @return --> vote count
 	def getVoteCount
-
 		VoteStore.get_vote(params[:replyid])
-
 	end
 
 	# precondition: both incoming reply and user ids are valid
 	# postcondition: updates the database, adds a vote to the reply,
 	#				 returns nothing
 	def addCount
-		
 		vote = {
 			"replyId" -> params[:replyid],
 			"userId" -> params[:userid],
 			"upvote" -> true
 		}
-
 		VoteStore.vote(params[:voteid], vote)
-
 	end
 
 	# @precondition: incoming reply id is valid
 	# @postcondition: updates the database, removing a vote from the reply,
 	#				  returns nothing
 	def deleteCount
-
 		VoteStore.remove_vote(params[:replyid])
-		
 	end
-
 end

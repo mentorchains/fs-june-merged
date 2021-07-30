291ffe71c44364e91891bb95ed201826ed7706f9
Nhien Lam <nhienlam.ricky@gmail.com>
2021-07-30 15:42:11 -0700

    setup basic structure for plugin widget

--- reply-voting/plugin.rb
+++ reply-voting/plugin.rb
@@ -1,3 +1,8 @@
 # name: reply-voting
 # version: 0.1
 # authors: fs-june-merged (team 1 + 2)
+
+register_asset 'stylesheets/common/reply-voting.scss'
+
+register_svg_icon "long-arrow-alt-up"
+register_svg_icon "long-arrow-alt-down"
